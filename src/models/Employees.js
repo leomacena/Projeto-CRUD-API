68d41412fff921adda16de5061ef0eb3e2f904ff
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2023-02-19 18:50:24 -0300

    Add files via upload

---
+++ src/models/Employees.js
@@ -0,0 +1,10 @@
+class Employee {
+    constructor(nome, cpf, email, cargo) {
+      this.nome = nome
+      this.cpf = cpf
+      this.email = email
+      this.cargo = cargo
+    }
+  }
+
+  module.exports = Employee
