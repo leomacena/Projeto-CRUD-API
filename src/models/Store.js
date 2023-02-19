44185c33db5577d096e6cc8f900324738aa3e443
Luís Henrique <109689914+Luis-Henrique-Lima@users.noreply.github.com>
2023-02-19 18:04:36 -0300

    Add files via upload

---
+++ src/models/Store.js
@@ -0,0 +1,13 @@
+class Store {
+    constructor(id, nome, email, telefone, responsavel) {
+      this.id = id
+      this.nome = nome
+      this.email = email
+      this.telefone = telefone
+      this.responsavel = responsavel
+    }
+  
+    
+  }
+  
+  module.exports = Store
