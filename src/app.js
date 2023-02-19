6eb0a9f06f87661f221ea8db4da6541d4e498e5a
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-19 11:59:36 -0300

    Configuração do app.jss
    Importando packages e controllers, configurando servidor, chamando rotas e exportando o module.

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// Importando o packages
+const express = require('express')
+
+// Importando o cors
+const cors = require('cors')
+
+// instanciando o servidor
+const app = express()
+
+// configurando o servidor para receber requisições com o corpo no formato JSON
+app.use(express.json())
+
+// Usado para aceitar requisições vindas de origens diferentes.
+app.use(cors())
+
+
+// importando os controllers
+const bookController = require('./controllers/book-controller.js')
+const customerController = require('./controllers/customer-controller.js')
+
+// Chama as rotas
+bookController.rotas(app)
+customerController.rotas(app)
+
+// Exporta o app para ser usado em outro módulo
+module.exports = app            
