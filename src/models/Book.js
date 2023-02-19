58025d910abef9382fca2a7d440272351786d1ed
pkdev95 <113390894+pkdev95@users.noreply.github.com>
2023-02-19 11:32:08 -0300

    Add files via upload
    Adiciona estrutura de classe modelo para ser utilizada nos arquivos controllers.

---
+++ src/models/Book.js
@@ -0,0 +1,13 @@
+class Book {
+    constructor(nome, autor, editora, idioma, paginas, ano){
+        this.nome = nome
+        this.autor = autor
+        this.editora = editora
+        this.idioma = idioma
+        this.paginas = paginas
+        this.ano = ano
+
+    }
+}
+
+module.exports = Book
---
+++ src/models/Customer.js
@@ -0,0 +1,14 @@
+class Customer{
+    constructor(id, nome, email, telefone, data_de_nascimento, cpf, senha){
+        this.id = id
+        this.nome = nome
+        this.email = email
+        this.telefone = telefone
+        this.data_de_nascimento = data_de_nascimento
+        this.cpf = cpf
+        this.senha = senha
+        
+    }
+}
+
+module.exports = Customer
