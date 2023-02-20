d040e1ce8c9a4b9456e5e1a893eb3ea37dcb6255
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-20 06:47:12 -0300

    Atualização de models

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -1,10 +1,15 @@
-class Employee {
-    constructor(nome, cpf, email, cargo) {
+class Employees {
+    constructor(nome, matricula, cargo, email, telefone, nascimento, cpf, senha, id_stores) {
       this.nome = nome
-      this.cpf = cpf
-      this.email = email
+      this.matricula = matricula
       this.cargo = cargo
+      this.email = email
+      this.telefone = telefone
+      this.nascimento = nascimento
+      this.cpf = cpf
+      this.senha = senha
+      this.id_stores = id_stores
     }
   }
 
-  module.exports = Employee
+  module.exports = Employees
