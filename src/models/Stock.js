922e9033c3820e1f5e089ceda42081a0040c8c06
pkdev95 <113390894+pkdev95@users.noreply.github.com>
2023-02-20 13:57:23 -0300

    Add files via upload

---
+++ src/models/Stock.js
@@ -0,0 +1,13 @@
+class Stock {
+    constructor(id_books, id_stores, quantidade){
+        this.id_books = id_books
+        this.id_stores = id_stores
+        this.quantidade = quantidade
+
+    }
+
+    
+    
+}
+
+module.exports = Stock
