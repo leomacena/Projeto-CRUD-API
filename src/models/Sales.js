40fe58467f1dc23835d80604b22e720ee6fa8f22
Leonardo Macena <113394226+leomacena@users.noreply.github.com>
2023-02-20 11:49:13 -0300

    Criação do model 'Sales.js'

---
+++ src/models/Sales.js
@@ -0,0 +1,12 @@
+class Sales {
+    constructor(id_customers, id_books, quantidade_adquirida, id_stores, data_compra, horario_compra) {
+      this.id_customers = id_customers
+      this.id_books = id_books
+      this.quantidade_adquirida = quantidade_adquirida
+      this.id_stores = id_stores
+      this.data_compra = data_compra
+      this.horario_compra =horario_compra
+    }
+  }
+  
+  module.exports = Sales
